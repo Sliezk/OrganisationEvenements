b5de73c132551bdf183126031b3b6d51b990c23f
Dylan MOREL <dylanmorel.perso@gmail.com>
2018-05-30 17:25:40 +0200

    Création BO Utilisateur

---
+++ BoEvents/Utilisateur.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoEvents
+{
+    public class Utilisateur
+    {
+
+        public Utilisateur()
+        {
+        }
+
+        public Utilisateur(Guid id, string nom, string prenom, string email, string adresse) : this()
+        {
+            this.ID = id;
+            this.Nom = nom;
+            this.Prenom = prenom;
+            this.Email = email;
+            this.Adresse = adresse;
+        }
+
+        public Guid ID { get; set; }
+
+        public string Nom { get; set; }
+
+        public string Prenom { get; set; }
+
+        public string Email { get; set; }
+
+        public string Adresse { get; set; }
+
+    }
+}
--- UnitTestProject1/DalTests.cs
+++ UnitTestProject1/DalTests.cs
@@ -25,5 +25,20 @@ namespace GestionEvenements
                 Assert.AreEqual("Thème random", evenements[0].Theme);
             }
         }
+
+        public void CreerUtilisateur_AvecUnNouvelUtilisateur_ObtientTousLesUtilisateursRenvoieBienLUtilisateur()
+        {
+            using (IDal dal = new Dal())
+            {
+                dal.CreerEvenement("Lieu random", new DateTime(2018, 06, 11, 12, 00, 00), 2, "Thème random");
+                List<Evenement> evenements = dal.GetListEvents();
+
+                Assert.IsNotNull(evenements);
+                Assert.AreEqual(1, evenements.Count);
+                Assert.AreEqual("Lieu random", evenements[0].Lieu);
+                Assert.AreEqual("Thème random", evenements[0].Theme);
+            }
+        }
+
     }
 }
